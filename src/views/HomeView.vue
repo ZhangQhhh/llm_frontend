1fe1fd4504c9c7fef32dc43a9b1a8cddb64c2780
ZhangQi <1529791593@qq.com>
2026-03-19 09:47:24 +0800

    1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -98,7 +98,6 @@
             <el-icon class="module-icon"><Reading /></el-icon>
             <span>边检智学</span>
           </router-link>
-          <router-link to="/smart-office-v2" class="btn btn-module">
           <router-link to="/smart-office" class="btn btn-module home-module-office">
             <span class="btn-shine"></span>
             <el-icon class="module-icon"><Document /></el-icon>
